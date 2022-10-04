e9d8972a85e70ff104b798b1037cfc073dcf1dec
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-04 08:38:36 -0400

    Updated server component styles and update

--- 2-course-project-the-basics/src/app/server/server.component.ts
+++ 2-course-project-the-basics/src/app/server/server.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.scss']
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent {
+  serverID: number = 10;
+  serverStatus: string = 'offline';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changeServerStatus = () => {
+    this.serverStatus = this.serverStatus === 'offline' ? 'online' : 'offline';
   }
-
 }
