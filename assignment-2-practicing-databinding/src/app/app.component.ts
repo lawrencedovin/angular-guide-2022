e0f7bf079d1b5e84d53c2bbb26dfe091cf140099
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-11 09:35:23 -0400

    Finished homework

--- assignment-2-practicing-databinding/src/app/app.component.ts
+++ assignment-2-practicing-databinding/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'assignment-2-practicing-databinding';
+  username = '';
+
+  resetUsername = () => {
+    this.username = '';
+  }
 }
