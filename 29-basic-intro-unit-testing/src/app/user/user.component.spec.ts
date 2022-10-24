9605029368bdd2b0e3e0823e2271878a5bfa2081
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-24 16:23:18 -0400

    Added basic unit test to check if app exists with a user component

--- 29-basic-intro-unit-testing/src/app/user/user.component.spec.ts
+++ 29-basic-intro-unit-testing/src/app/user/user.component.spec.ts
@@ -1,25 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [UserComponent]
+    })
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create the app', () => {
+    // Creating the User Component
+    let fixture = TestBed.createComponent(UserComponent);
+    // debugElement is the Element exposed to us for testing purposes
+    let app = fixture.debugElement.componentInstance;
+    // Tests if the application exists and is starting up
+    expect(app).toBeTruthy();
+  })
 });
--- 29-basic-intro-unit-testing/src/app/user/user.component.ts
+++ 29-basic-intro-unit-testing/src/app/user/user.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  user: {name: string};
+  isLoggedIn = false;
 
   constructor() { }
 
