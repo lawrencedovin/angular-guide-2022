2c9871fda1dcbbc76d9651925cb9829ee5d634c1
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-25 08:23:05 -0400

    Added async tests

--- 29-basic-intro-unit-testing/src/app/user/user.component.spec.ts
+++ 29-basic-intro-unit-testing/src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 describe('UserComponent', () => {
   beforeEach(() => {
@@ -63,5 +64,50 @@ describe('UserComponent', () => {
     expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
   });
 
-  it(`shouldn't fetch data successfully if not called asynchronously`,)
+  it(`should fetch data successfully if called asynchronously`, waitForAsync (() => {
+    // Creating the User Component
+    let fixture = TestBed.createComponent(UserComponent);
+    // debugElement is the Element exposed to us for testing purposes
+    let app = fixture.debugElement.componentInstance;
+    // Uses Angular Injector and gets instance of DataService
+    // Forces DataService to be injected into the Component in testing environment
+    let dataService = fixture.debugElement.injector.get(DataService);
+    // spyOn means we get informed/listen everytime getDetails() gets executed.
+    // When getDetails() gets executed when running a test, we won't execute it
+    // and instead we will return a value on our own.
+    // 1st argument is what we want to spy on, 2nd argument is what method we
+    // want to spy on within the dataService.
+    // Promise.resolve will execute the async code and will eventually give us
+    // back our own Data and not what the async task would have given us.
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    // Updates Component after getting our own Promise.resolve('Data') back.
+    fixture.detectChanges();
+    // Reacts to all asynchronous tasks that are finished
+    fixture.whenStable().then(() => {
+      expect(app.data).toBe('Data');
+    });
+  }));
+
+  it(`shouldn't fetch data successfully if not called asynchronously`, () => {
+    // Creating the User Component
+    let fixture = TestBed.createComponent(UserComponent);
+    // debugElement is the Element exposed to us for testing purposes
+    let app = fixture.debugElement.componentInstance;
+    // Uses Angular Injector and gets instance of DataService
+    // Forces DataService to be injected into the Component in testing environment
+    let dataService = fixture.debugElement.injector.get(DataService);
+    // spyOn means we get informed/listen everytime getDetails() gets executed.
+    // When getDetails() gets executed when running a test, we won't execute it
+    // and instead we will return a value on our own.
+    // 1st argument is what we want to spy on, 2nd argument is what method we
+    // want to spy on within the dataService.
+    // Promise.resolve will execute the async code and will eventually give us
+    // back our own Data and not what the async task would have given us.
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    // Updates Component after getting our own Promise.resolve('Data') back.
+    fixture.detectChanges();
+    expect(app.data).toBe(undefined);
+  });
 });
