c1c008991ce698a020d0d01acf6340ed9355b2d6
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-27 16:42:36 -0400

    setted up onSetToInactive and onSetToActive to use Users Service

--- services-assignment/src/app/app.component.ts
+++ services-assignment/src/app/app.component.ts
@@ -18,12 +18,10 @@ export class AppComponent implements OnInit {
   }
 
   onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
+    this.usersService.setToInactive(id);
   }
 
   onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+    this.usersService.setToActive(id);
   }
 }
