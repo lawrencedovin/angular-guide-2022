90687c0a071ab78eaf20563953a74272298f8707
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-27 08:30:13 -0400

    Added custom directive example

--- 7-directives-deep-dive/src/app/app.module.ts
+++ 7-directives-deep-dive/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './directives/basic-highlight.directive';
+import { BetterHighlightDirective } from './directives/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
