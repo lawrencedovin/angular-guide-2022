5319ca8106f4652734444c55bbc726c32b35ed1c
Lawrence Dovin <lawrencedovin@gmail.com>
2022-10-27 08:44:41 -0400

    Added HostListener example

--- 7-directives-deep-dive/src/app/directives/better-highlight.directive.ts
+++ 7-directives-deep-dive/src/app/directives/better-highlight.directive.ts
@@ -1,17 +1,37 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
-  ngOnInit(): void {
-      this.renderer.setStyle(
-        this.elementRef.nativeElement,
-        'background-color',
-        'blue'
-      )
+  // Listens to the mouseenter Event
+  // Gets the eventData
+  @HostListener('mouseenter') mouseenter(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'blue'
+    )
   }
+
+  // Listens to the mouseleave Event
+  // Gets the eventData
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'transparent'
+    )
+  }
+
+  ngOnInit(): void {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // )
+}
 }
