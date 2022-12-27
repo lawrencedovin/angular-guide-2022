83abec0a764966ef1ce3ac687ae2ab2903f1b7a6
Lawrence Dovin <lawrencedovin@gmail.com>
2022-12-27 11:21:58 -0500

    Fixed Page Reload issues

--- cook-book/src/app/app-routing.module.ts
+++ cook-book/src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { RecipeBookComponent } from './modules/recipe-book/recipe-book/recipe-bo
 import { ShoppingListComponent } from './modules/shopping-list/shopping-list/shopping-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'recipe', pathMatch: 'full' },
-  { path: 'recipe', component: RecipeBookComponent },
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipeBookComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: '**', redirectTo: 'recipe' }
+  { path: '**', redirectTo: 'recipes' }
 ];
 
 @NgModule({
