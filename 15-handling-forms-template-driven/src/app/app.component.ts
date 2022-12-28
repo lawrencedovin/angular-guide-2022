1d33653fcb874667123fa9afe441cad54afc8a37
Lawrence Dovin <lawrencedovin@gmail.com>
2022-12-28 16:13:02 -0500

    Added form validation and default select value

--- 15-handling-forms-template-driven/src/app/app.component.ts
+++ 15-handling-forms-template-driven/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('userForm') userForm: NgForm
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(userForm: NgForm) {
+  //   console.log('User form values: ', userForm.value);
+  //   console.log('Valid: ', userForm.valid);
+  // }
+
+  onSubmit() {
+    console.log('User form values: ', this.userForm.value);
+    console.log('Valid: ', this.userForm.valid);
   }
 }
