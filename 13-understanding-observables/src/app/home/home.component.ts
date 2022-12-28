e4473b416921c659377acdb09301b43be6405134
Lawrence Dovin <lawrencedovin@gmail.com>
2022-12-28 10:58:57 -0500

    Added observable operators example

--- 13-understanding-observables/src/app/home/home.component.ts
+++ 13-understanding-observables/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -31,11 +32,18 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.firstObservableSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObservableSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) =>
+    {
+      return `Round: ${(data + 1)}`;
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
       alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 
