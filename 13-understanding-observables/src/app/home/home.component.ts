4b1558eb229ec640c93e286b5c96ed0b23073b75
Lawrence Dovin <lawrencedovin@gmail.com>
2022-12-28 09:57:18 -0500

    Added observer handler example

--- 13-understanding-observables/src/app/home/home.component.ts
+++ 13-understanding-observables/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +16,27 @@ export class HomeComponent implements OnInit, OnDestroy {
     // We get a new value every second that we emit to the console.
     // The Observable is interval(1000).
     // Subscribe gets the new value emitted from the Observable interval(1000).
-    this.firstObservableSubscription = interval(1000).subscribe(count => console.log(count));
+    // this.firstObservableSubscription = interval(1000).subscribe(count => console.log(count));
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count === 2) {
+          observer.complete();
+        }
+        if(count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObservableSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    });
   }
 
   ngOnDestroy(): void {
