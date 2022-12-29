3debc0ff0328fcb085ec7934dbf24b2c810eb135
Lawrence Dovin <lawrencedovin@gmail.com>
2022-12-29 13:50:59 -0500

    Added solution

--- assignment-6-template-driven-forms/src/app/app.component.ts
+++ assignment-6-template-driven-forms/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
+  @ViewChild('subscriptionForm') subscriptionForm: NgForm;
+  subscriptionOptions = [
+    {
+      text: 'Basic',
+      value: 'basic'
+    },
+    {
+      text: 'Advanced',
+      value: 'advanced'
+    },
+    {
+      text: 'Pro',
+      value: 'pro'
+    }
+  ]
+  defaultSubscription = 'advanced'
+  userSubscription = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+  submitted = false;
+
+  resetForm() {
+    this.subscriptionForm.reset();
+    this.subscriptionForm.form.patchValue(
+    {
+      subscriptions: 'advanced'
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.userSubscription.email = this.subscriptionForm.value.email;
+    this.userSubscription.subscription = this.subscriptionForm.value.subscriptions;
+    this.userSubscription.password = this.subscriptionForm.value.password;
+    this.resetForm();
+  }
 }
