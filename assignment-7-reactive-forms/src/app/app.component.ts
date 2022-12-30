a2b74002e3bb1f6d260fc0d5458e72e39cba69e7
Lawrence Dovin <lawrencedovin@gmail.com>
2022-12-30 10:54:10 -0500

    Added assignment 7 solution

---
+++ assignment-7-reactive-forms/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, AsyncValidatorFn } from '@angular/forms';
+import { FormValidatorsService } from 'src/services/form-validators.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  signupForm: FormGroup;
+  statusesOptions = [
+    {
+      'value': 'stable',
+      'text': 'Stable'
+    },
+    {
+      'value': 'critical',
+      'text': 'Critical'
+    },
+    {
+      'value': 'finished',
+      'text': 'Finished'
+    }
+  ];
+
+  constructor(private formValidatorsService: FormValidatorsService) {}
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'projectData': new FormGroup({
+        //'projectName': new FormControl(null, [Validators.required, this.formValidatorsService.forbiddenName.bind(this)]),
+        'projectName': new FormControl(null, Validators.required, this.formValidatorsService.asyncForbiddenName as AsyncValidatorFn),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'statuses': new FormControl('critical')
+    });
+  }
+
+  resetForm() {
+    this.signupForm.reset();
+    this.signupForm.patchValue({
+      'statuses': 'critical'
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.resetForm();
+  }
+
+}
---
+++ assignment-7-reactive-forms/src/services/form-validators.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormValidatorsService {
+
+  constructor() { }
+
+  forbiddenName(control: FormControl): {[key: string]: boolean} | null {
+    if(control.value === 'Test') return {'projectNameIsForbidden': true};
+    return null;
+  }
+
+  asyncForbiddenName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'Test') {
+          resolve({'projectNameIsForbidden': true});
+        }
+        else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+}
