d18f1265b932ff0fb53a40ac122532e0048bdbdd
Lawrence Dovin <lawrencedovin@gmail.com>
2023-01-27 15:17:25 -0500

    Added post request

--- 18-http-requests/src/app/app.component.ts
+++ 18-http-requests/src/app/app.component.ts
@@ -1,28 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+
+interface PostData {
+  title: string
+  content: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
   loadedPosts = [];
+  baseUrl = 'https://ng-complete-guide-b3e29-default-rtdb.firebaseio.com';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: PostData) {
     // Send Http request
-    console.log(postData);
+    this.subscription = this.http
+      .post<PostData>(this.baseUrl + '/posts.json', postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+    });
   }
-
   onFetchPosts() {
     // Send Http request
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
   }
 }
