fc784783ad06ad966572c183bf66de1cafede744
Lawrence Dovin <lawrencedovin@gmail.com>
2023-01-27 09:21:55 -0500

    Added reverse pipe

---
+++ assignment-8-pipes/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { ShortenPipe } from 'src/pipes/shorten.pipe';
+import { FilterServersPipe } from 'src/pipes/filter-servers.pipe';
+import { ReversePipe } from 'src/pipes/reverse.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShortenPipe,
+    FilterServersPipe,
+    ReversePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
