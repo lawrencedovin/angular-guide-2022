dc6b237fbf8432da8dce3ac9ec31d15599a3cbae
Lawrence Dovin <lawrencedovin@gmail.com>
2023-03-24 08:43:10 -0400

    Fixed resolver bug to load current recipes for changes instead of getting new recipes from server

--- cook-book/src/app/services/recipes-resolver.service.ts
+++ cook-book/src/app/services/recipes-resolver.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Recipe } from '../models/recipe.model';
 import { DataStorageService } from './data-storage.service';
+import { RecipesService } from './recipes.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService,
+              private recipesService: RecipesService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipesService.getRecipes();
+
+    if(recipes.length === 0) return this.dataStorageService.fetchRecipes();
+    return recipes;
   }
 }
