148e21b0a53b0498dfd2105c7771344fd37e6f0c
Lawrence Dovin <lawrencedovin@gmail.com>
2023-09-26 12:57:54 -0400

    Added interceptor code

--- cook-book/src/app/app-routing.module.ts
+++ cook-book/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
@@ -6,31 +7,43 @@ import { RecipeStartComponent } from './components/recipe-start/recipe-start.com
 import { RecipesEditComponent } from './components/recipes-edit/recipes-edit.component';
 import { RecipeBookComponent } from './modules/recipe-book/recipe-book/recipe-book.component';
 import { ShoppingListComponent } from './modules/shopping-list/shopping-list/shopping-list.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 import { RecipesResolverService } from './services/recipes-resolver.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipeBookComponent, children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'new', component: RecipesEditComponent },
-    {
-      path: ':id',
-      component: RecipeDetailComponent,
-      resolve: [RecipesResolverService]
-    },
-    {
-      path: ':id/edit',
-      component: RecipesEditComponent,
-      resolve: [RecipesResolverService]
-    }
-  ]},
+  {
+    path: 'recipes',
+    component: RecipeBookComponent,
+    children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipesEditComponent },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [RecipesResolverService],
+      },
+      {
+        path: ':id/edit',
+        component: RecipesEditComponent,
+        resolve: [RecipesResolverService],
+      },
+    ],
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
-  { path: '**', redirectTo: 'recipes' }
+  { path: '**', redirectTo: 'recipes' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- cook-book/src/app/services/data-storage.service.ts
+++ cook-book/src/app/services/data-storage.service.ts
@@ -28,27 +28,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap((user) => {
-        return this.http.get<Recipe[]>(
-          'https://ng-cook-book-d23c0-default-rtdb.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
-      map((recipes) => {
-        return recipes.map((recipe) => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : [],
-          };
-        });
-      }),
-      tap((recipes) => {
-        this.recipesService.setRecipes(recipes);
-      })
-    );
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-cook-book-d23c0-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipesService.setRecipes(recipes);
+        })
+      );
   }
 }
