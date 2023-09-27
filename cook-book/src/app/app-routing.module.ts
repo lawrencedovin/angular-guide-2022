2caf1dd12f3d714ded3d8580d3ab6149a17f4f89
Lawrence Dovin <lawrencedovin@gmail.com>
2023-09-27 09:15:53 -0400

    Added AuthGuard to prevent user that isn't authenticated to go to recipes page

--- cook-book/src/app/app-routing.module.ts
+++ cook-book/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './components/recipe-start/recipe-start.component';
 import { RecipesEditComponent } from './components/recipes-edit/recipes-edit.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   {
     path: 'recipes',
     component: RecipeBookComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipesEditComponent },
